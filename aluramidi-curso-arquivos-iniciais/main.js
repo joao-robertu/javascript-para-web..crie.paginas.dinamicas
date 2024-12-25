cc90babff357138e761a62f5de43d5ed0bae0a12
João Roberto <joao.adsbrasil@gmail.com>
2024-12-25 20:30:00 -0300

    Condições no código

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -17,8 +17,13 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function (evento) {
+        
+        console.log(evento.code === 'Space')
+
+        if (evento.code =='Space') {
+            tecla.classList.add('ativa');
+        } else  
     }
 
     tecla.onkeyup = function() {
